3554d15cf73debd0983f4d22aafbbcbf0bfaf13d
h321606825 <one_start@nefu.edu.cn>
2020-04-09 14:30:54 +0800

    用户详情页

--- app/Http/Controllers/Api/User/InfoController.php
+++ app/Http/Controllers/Api/User/InfoController.php
@@ -5,8 +5,34 @@ namespace App\Http\Controllers\Api\User;
 
 
 use App\Http\Controllers\Controller;
+use Service\User\UserService;
+use Unit\Checkunit;
 
 class InfoController extends Controller
 {
+    /**
+     * 学员详情页
+     * @return InfoController
+     */
+    public function stuInfo(){
+        $stuid = $this->req['id'];
 
+        Checkunit::verification($stuid,4,'学生id',strlen($stuid));
+        $data = UserService::selectStuByPhone($stuid);
+
+        return $this->resp($data);
+    }
+
+    /**
+     * 教员详情页
+     * @return InfoController
+     */
+    public function teaInfo(){
+        $stuid = $this->req['id'];
+
+        Checkunit::verification($stuid,4,'教师id',strlen($stuid));
+        $data = UserService::selectTeaByPhone($stuid);
+
+        return $this->resp($data);
+    }
 }
